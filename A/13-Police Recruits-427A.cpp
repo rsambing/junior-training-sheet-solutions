20171059da4be9064f11084a5b744b5b19e48f17
rsambing <reinaldogts252@gmail.com>
2025-06-11 20:45:31 +0100

    solved

--- A/13-Police Recruits-427A.cpp
+++ A/13-Police Recruits-427A.cpp
@@ -6,5 +6,16 @@ using namespace std;
 
 int main(void)
 {
+    int n, ans = 0, polices = 0; cin >> n;
+    int arr[n]; for (int i = 0; i < n; i++) cin >> arr[i];
 
+    for (int i = 0; i < n; i++) 
+    {
+        if ((arr[i] != -1))
+            polices += arr[i];
+        else
+            ans += polices > 0 ? (polices--, 0) : 1;
+    }
+    cout << ans << endl;
+    return (0);
 }
