d1fb6f62f1a4d0acb8c611dbc913b2b7e46511a8
rsambing <reinaldogts252@gmail.com>
2025-06-13 20:19:28 +0100

    done\!

--- A/10-Magnets-344.cpp
+++ A/10-Magnets-344.cpp
@@ -2,7 +2,14 @@
 
 #include <bits/stdc++.h>
 
+using namespace std;
+
 int main()
 {
+    int n, ans = 1; cin >> n;
+    string row = "", aux;
+    for(int i = 0; i < n; i++) (cin >> aux, row += aux);
+    for(int i = 0; i < (n * 2) - 2; i++) if(row[i] == row[i + 1]) ans++;
+    cout << ans << endl; 
     return (0);
 }
