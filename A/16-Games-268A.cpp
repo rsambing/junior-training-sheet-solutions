bb24e167ade2c4d53cba1d17a365f68c50b2e69b
rsambing <reinaldogts252@gmail.com>
2025-06-16 19:48:36 +0100

    done

---
+++ A/16-Games-268A.cpp
@@ -0,0 +1,20 @@
+// https://codeforces.com/contest/268/problem/A
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void)
+{
+    int ans = 0, n; cin >> n;
+    vector<int> a;
+    unordered_map<int, int> mp;
+
+    while(n--)
+    {
+        int first; cin >> first; a.push_back(first);
+        int second; cin >> second; mp[second]++;
+    }
+    for(size_t i = 0; i < a.size(); i++) ans += mp[a[i]];
+    cout << ans << endl;
+}
