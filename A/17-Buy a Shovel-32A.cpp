353c1ed02ee04ec4296798f8c3e6dcbdd957c949
rsambing <reinaldogts252@gmail.com>
2025-06-18 21:48:26 +0100

    I'll review this later

--- A/17-Buy a Shovel-32A.cpp
+++ A/17-Buy a Shovel-32A.cpp
@@ -1,9 +1,20 @@
 // http://codeforces.com/contest/732/problem/A
 
-#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
 
 int main(void)
 {
-    
-    return (0);
+    int k, r;
+    cin >> k >> r;
+
+    for (int i = 1; i <= 10; i++) {
+        int total = i * k;
+        if (total % 10 == 0 || total % 10 == r) {
+            cout << i << endl;
+            break;
+        }
+    }
+
+    return 0;
 }
