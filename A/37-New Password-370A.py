7b799fe45a14523061ebec88a15252b51230f6e1
Reinaldo Sambinga <rsambing@c1r5s7.42luanda.com>
2025-08-04 10:13:14 +0100

    done

---
+++ A/37-New Password-370A.py
@@ -0,0 +1,14 @@
+arr = list(map(int, input().split()))
+n = arr[0]
+k = arr[1]
+
+dist = "".join(chr(c) for c in range(ord('a'), ord('a') + k))
+
+j = 0
+password = ""
+
+for i in range(n):
+    password += dist[j]
+    j = (j + 1) % k
+
+print(password)
